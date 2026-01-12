159357c440a9858f90bbf488a788251e8c9eefcf
NhatVinh2005 <vinhnhat896@gmail.com>
2026-01-13 00:13:07 +0700

    uia
    Complete Wed_3_b

--- Wed_03_b/src/java/model/UserDAO.java
+++ Wed_03_b/src/java/model/UserDAO.java
@@ -4,32 +4,53 @@
  */
 package model;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
+import utils.DBUtils;
 
 /**
  *
  * @author THIS PC
  */
 public class UserDAO {
+
     public ArrayList<UserDTO> list = new ArrayList<>();
 
     public UserDAO() {
-        list = new ArrayList<>();
-        list.add(new UserDTO("admin","admin","Nguyen Van A"));
-        list.add(new UserDTO("user1","user1","Nguyen Van B"));
     }
-    
-    public UserDTO searchById(String username){
-        for (UserDTO userDTO : list){
-            if (userDTO.getUsername().equalsIgnoreCase(username))
-                return userDTO;
+
+    public UserDTO searchById(String username) {
+
+        try {
+            Connection conn = DBUtils.getConnection();
+            String sql = "SELECT * FROM tblUsers "
+                    + " WHERE userID='" + username + "'";
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+
+            UserDTO user = null;
+
+            while (rs.next()) {
+                String userID = rs.getString("userID");
+                String fullName = rs.getString("fullName");
+                String password = rs.getString("password");
+                String roleID = rs.getString("roleID");
+                boolean status = rs.getBoolean("status");
+                user = new UserDTO(userID, fullName, password, roleID, status);
+            }
+
+            return user;
+        } catch (Exception e) {
+            return null;
         }
-        return null;
+
     }
-    
-    public UserDTO login(String username, String password){
+
+    public UserDTO login(String username, String password) {
         UserDTO u = searchById(username);
-        if(u != null && u.getPassword().equals(password)){
+        if (u != null && u.getPassword().equals(password)) {
             return u;
         }
         return null;
